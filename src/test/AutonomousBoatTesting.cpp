ba13b5cd65b894ce0da10d7a0c283baaa6af143e
ben <ben@bens-MacBook-Pro-2.local>
2019-05-06 18:51:50 -0400

    fixed tests

--- src/test/AutonomousBoatTesting.cpp
+++ src/test/AutonomousBoatTesting.cpp
@@ -6,24 +6,17 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
-//#include <iostream>
-//#include "catch.hpp"
-//
-//int main() {
-//	std::cout << "!!!Hello World!!!" << std::endl; // prints !!!Hello World!!!
-//	return 0;
-//}
 
 #include <stdio.h>
 #include <string.h>
 
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
-#include "TestGPS.cpp"
-#include "../GPSParser.cpp"
+#include "test/TestGPS.cpp"
+#include "GPSParser.cpp"
 
 
-TEST_CASE("Factorials are computed", "[factorial]" ) {
+TEST_CASE("get latitude Latitude", "[GPSParser]" ) {
 	const char * nmeaSentence = "$GPRMC,144326.00,A,5107.0017737,N,11402.3291611,W,0.080,323.3,210307,0.0,E,A*20";
 	TestGPS * testGPS = new TestGPS(nmeaSentence);
 	GPSParser gpsParser(testGPS);
@@ -31,7 +24,7 @@ TEST_CASE("Factorials are computed", "[factorial]" ) {
 	float expectedLatitude = 5107.0017737f;
 
 	struct GPSCoordinates actualGPSCoordinates;
-	actualGPSCoordinates = gpsParser.waitAndGetNextNmeaSentence();
+	actualGPSCoordinates = gpsParser.waitAndGetNextPosition();
 	float actualLatitude = actualGPSCoordinates.latitude;
     REQUIRE(actualLatitude ==  expectedLatitude);
 
