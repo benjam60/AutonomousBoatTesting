3aaa9be4c69ee7a1f18f23b4e8e0553b49a7364d
ben <ben@bens-MacBook-Pro-2.local>
2019-05-12 18:47:27 -0400

    second test

--- src/test/AutonomousBoatTesting.cpp
+++ src/test/AutonomousBoatTesting.cpp
@@ -5,22 +5,20 @@
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
-
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
 #include <stdio.h>
 #include <string.h>
-
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "test/TestGPS.cpp"
 #include "GPSParser.cpp"
 
 //check for valid chars as well as valid #s...e.g. number range as well
 TEST_CASE("Get latitude", "[GPSParser]" ) {
-	char validNmeaWithLatitude[1][150];
-	strcpy(validNmeaWithLatitude[0], "$GPRMC,144326.00,A,5107.0017737,N,11402.3291611,W,0.080,323.3,210307,0.0,E,A*20");
-	char ** input = (char **)validNmeaWithLatitude;
-	TestGPS * testGPS = new TestGPS(input, 2);
+	typedef char NmeaMessageArray[1][150];
+	NmeaMessageArray messages;
+	strcpy(messages[0], "$GPRMC,144326.00,A,5107.0017737,N,11402.3291611,W,0.080,323.3,210307,0.0,E,A*20");
+	TestGPS * testGPS = new TestGPS(messages, 1);
 	GPSParser gpsParser(testGPS);
 
 	float expectedLatitude = 5107.0017737f;
@@ -32,12 +30,12 @@ TEST_CASE("Get latitude", "[GPSParser]" ) {
     REQUIRE(actualLatitude ==  expectedLatitude);
 }
 
-TEST_CASE("Ignore missing latitude", "[GPSParser]" ) {
-	char badAndThenGoodInput[2][150];
-	strcpy(&badAndThenGoodInput[0][0], "$GPRMC,144326.00,A,,N,11402.3291611,W,0.080,323.3,210307,0.0,E,A*20");
-	strcpy(&badAndThenGoodInput[1][0], "$GPRMC,144326.00,A,5107.0017737,N,11402.3291611,W,0.080,323.3,210307,0.0,E,A*20");
-	char ** input = (char **)badAndThenGoodInput;
-	TestGPS * testGPS = new TestGPS(input, 2);
+TEST_CASE("Ignore messages with missing latitude", "[GPSParser]" ) {
+	typedef char NmeaMessageArray[2][150];
+	NmeaMessageArray badAndThenGoodInput;
+	strcpy(badAndThenGoodInput[0], "$GPRMC,144326.00,A,,N,11402.3291611,W,0.080,323.3,210307,0.0,E,A*20");
+	strcpy(badAndThenGoodInput[1], "$GPRMC,144326.00,A,5107.0017737,N,11402.3291611,W,0.080,323.3,210307,0.0,E,A*20");
+	TestGPS * testGPS = new TestGPS(badAndThenGoodInput, 2);
 	GPSParser gpsParser(testGPS);
 
 	float expectedLatitude = 5107.0017737f;
