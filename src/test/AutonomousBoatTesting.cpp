77ffb28e4d15a7e1ac09ffc17ae473506b6a4b72
ben <ben@bens-MacBook-Pro-2.local>
2019-05-12 22:43:58 -0400

    added longitude test as well as failing validity test

--- src/test/AutonomousBoatTesting.cpp
+++ src/test/AutonomousBoatTesting.cpp
@@ -16,6 +16,9 @@
 //check for valid chars as well as valid #s...e.g. number range as well
 //TODO: feed the gps lots of crap data and try to get it to segfault
 
+
+//Latitude
+
 TEST_CASE("Get latitude", "[GPSParser]" ) {
 	typedef char NmeaMessageArray[1][150];
 	NmeaMessageArray messages;
@@ -84,10 +87,44 @@ TEST_CASE("handle incomplete $GPRMC message", "[GPSParser]" ) {
 }
 
 
+//Longitude
+
+TEST_CASE("get longitude from $GPRMC message", "[GPSParser]" ) {
+	typedef char NmeaMessageArray[1][150];
+	NmeaMessageArray messages;
+	strcpy(messages[0], "$GPRMC,144326.00,A,5107.0017737,N,11402.3291611,W,0.080,323.3,210307,0.0,E,A*20");
+	TestGPS * testGPS = new TestGPS(messages, 1);
+	GPSParser gpsParser(testGPS);
+
+	float expectedLongitude = 11402.3291611f;
+	struct GPSCoordinates actualGPSCoordinates;
+	actualGPSCoordinates = gpsParser.waitAndGetNextPosition();
+	float actualLongitude = actualGPSCoordinates.longitude;
+	REQUIRE(actualLongitude ==  expectedLongitude);
+}
+
+
+
+//Bad messages
+TEST_CASE("if the message status is V then we should ignore", "[GPSParser]" ) {
+	typedef char NmeaMessageArray[1][150];
+	NmeaMessageArray messages;
+	strcpy(messages[0], "$GPRMC,144326.00,A,5107.0017737,N,11402.3291611,W,0.080,323.3,210307,0.0,E,A*20");
+	TestGPS * testGPS = new TestGPS(messages, 1);
+	GPSParser gpsParser(testGPS);
+
+	float expectedLongitude = 11402.3291611f;
+	struct GPSCoordinates actualGPSCoordinates;
+	actualGPSCoordinates = gpsParser.waitAndGetNextPosition();
+	float actualLongitude = actualGPSCoordinates.longitude;
+	REQUIRE(actualLongitude ==  expectedLongitude);
+}
+
+
 //TEST_CASE("handle invalid character e.g. control characters in middle of message", "[GPSParser]" ) {
 //	typedef char NmeaMessageArray[2][150];
 //	NmeaMessageArray incompleteMessage;
-//	strcpy(incompleteMessage[0], "$GPRMC,144326.00,A,4007.0017737\t,N,11402.3291611,W,0.080,323.3");
+//	strcpy(incompleteMessage[0], "$GPRMC,144326.00,A,4\x01\x02\x03\x04,N,11402.3291611,W,0.080,323.3,210307,0.0,E,A*20");
 //	strcpy(incompleteMessage[1], "$GPRMC,144326.00,A,4107.0017737,N,11402.3291611,W,0.080,323.3,210307,0.0,E,A*20");
 //	TestGPS * testGPS = new TestGPS(incompleteMessage, 2);
 //	GPSParser gpsParser(testGPS);
