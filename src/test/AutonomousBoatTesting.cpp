57bcf2c79723888a371072a8ca20d47a76666a6b
ben <ben@bens-MacBook-Pro-2.local>
2019-05-12 21:59:10 -0400

    more tests

--- src/test/AutonomousBoatTesting.cpp
+++ src/test/AutonomousBoatTesting.cpp
@@ -14,6 +14,8 @@
 #include "GPSParser.cpp"
 
 //check for valid chars as well as valid #s...e.g. number range as well
+//TODO: feed the gps lots of crap data and try to get it to segfault
+
 TEST_CASE("Get latitude", "[GPSParser]" ) {
 	typedef char NmeaMessageArray[1][150];
 	NmeaMessageArray messages;
@@ -46,7 +48,6 @@ TEST_CASE("Ignore messages with missing latitude", "[GPSParser]" ) {
 
     REQUIRE(actualLatitude ==  expectedLatitude);
 }
-
 
 TEST_CASE("Only parse messages for longitude with messages that begin with $GPRMC", "[GPSParser]" ) {
 	typedef char NmeaMessageArray[2][150];
@@ -81,6 +82,25 @@ TEST_CASE("handle incomplete $GPRMC message", "[GPSParser]" ) {
 
     REQUIRE(actualLatitude ==  expectedLatitude);
 }
+
+
+//TEST_CASE("handle invalid character e.g. control characters in middle of message", "[GPSParser]" ) {
+//	typedef char NmeaMessageArray[2][150];
+//	NmeaMessageArray incompleteMessage;
+//	strcpy(incompleteMessage[0], "$GPRMC,144326.00,A,4007.0017737\t,N,11402.3291611,W,0.080,323.3");
+//	strcpy(incompleteMessage[1], "$GPRMC,144326.00,A,4107.0017737,N,11402.3291611,W,0.080,323.3,210307,0.0,E,A*20");
+//	TestGPS * testGPS = new TestGPS(incompleteMessage, 2);
+//	GPSParser gpsParser(testGPS);
+//
+//	float expectedLatitude = 4107.0017737f;
+//
+//	struct GPSCoordinates actualGPSCoordinates;
+//	actualGPSCoordinates = gpsParser.waitAndGetNextPosition();
+//	float actualLatitude = actualGPSCoordinates.latitude;
+//
+//    REQUIRE(actualLatitude ==  expectedLatitude);
+//}
+
 
 
 
