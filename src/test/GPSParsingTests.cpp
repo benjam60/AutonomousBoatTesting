21e35964120549bff89ea15a1386ee275a75a74d
ben <ben@bens-MacBook-Pro-2.local>
2019-05-13 19:09:20 -0400

    testing direction

--- src/test/GPSParsingTests.cpp
+++ src/test/GPSParsingTests.cpp
@@ -3,15 +3,16 @@
 // Author      : Ben
 // Version     :
 // Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Description : Testing my boat's gps parsing
 //============================================================================
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "catch.hpp"
-#include "test/TestGPS.cpp"
-#include "GPSParser.cpp"
+#include <test/TestGPS.cpp>
+#include <GPSParser.cpp>
 
 //check for valid chars as well as valid #s...e.g. number range as well
 //TODO: feed the gps lots of crap data and try to get it to segfault
@@ -26,13 +27,10 @@ TEST_CASE("Get latitude", "[GPSParser]" ) {
 	TestGPS * testGPS = new TestGPS(messages, 1);
 	GPSParser gpsParser(testGPS);
 
-	float expectedLatitude = 5107.0017737f;
-
 	struct GPSCoordinates actualGPSCoordinates;
 	actualGPSCoordinates = gpsParser.waitAndGetNextPosition();
-	float actualLatitude = actualGPSCoordinates.latitude;
 
-    REQUIRE(actualLatitude ==  expectedLatitude);
+    REQUIRE(5107.0017737f == actualGPSCoordinates.latitude);
 }
 
 TEST_CASE("Ignore messages with missing latitude", "[GPSParser]" ) {
@@ -43,13 +41,10 @@ TEST_CASE("Ignore messages with missing latitude", "[GPSParser]" ) {
 	TestGPS * testGPS = new TestGPS(badAndThenGoodInput, 2);
 	GPSParser gpsParser(testGPS);
 
-	float expectedLatitude = 5107.0017737f;
-
 	struct GPSCoordinates actualGPSCoordinates;
 	actualGPSCoordinates = gpsParser.waitAndGetNextPosition();
-	float actualLatitude = actualGPSCoordinates.latitude;
 
-    REQUIRE(actualLatitude ==  expectedLatitude);
+    REQUIRE(5107.0017737f == actualGPSCoordinates.latitude);
 }
 
 TEST_CASE("Only parse messages for longitude with messages that begin with $GPRMC", "[GPSParser]" ) {
@@ -60,13 +55,10 @@ TEST_CASE("Only parse messages for longitude with messages that begin with $GPRM
 	TestGPS * testGPS = new TestGPS(badAndThenGoodInput, 2);
 	GPSParser gpsParser(testGPS);
 
-	float expectedLatitude = 5107.0017737f;
-
 	struct GPSCoordinates actualGPSCoordinates;
 	actualGPSCoordinates = gpsParser.waitAndGetNextPosition();
-	float actualLatitude = actualGPSCoordinates.latitude;
 
-    REQUIRE(actualLatitude ==  expectedLatitude);
+    REQUIRE(5107.0017737f == actualGPSCoordinates.latitude);
 }
 
 TEST_CASE("handle incomplete $GPRMC message", "[GPSParser]" ) {
@@ -77,13 +69,10 @@ TEST_CASE("handle incomplete $GPRMC message", "[GPSParser]" ) {
 	TestGPS * testGPS = new TestGPS(incompleteMessage, 2);
 	GPSParser gpsParser(testGPS);
 
-	float expectedLatitude = 4107.0017737f;
-
 	struct GPSCoordinates actualGPSCoordinates;
 	actualGPSCoordinates = gpsParser.waitAndGetNextPosition();
-	float actualLatitude = actualGPSCoordinates.latitude;
 
-    REQUIRE(actualLatitude ==  expectedLatitude);
+    REQUIRE(4107.0017737f == actualGPSCoordinates.latitude);
 }
 
 
@@ -96,48 +85,108 @@ TEST_CASE("get longitude from $GPRMC message", "[GPSParser]" ) {
 	TestGPS * testGPS = new TestGPS(messages, 1);
 	GPSParser gpsParser(testGPS);
 
-	float expectedLongitude = 11402.3291611f;
 	struct GPSCoordinates actualGPSCoordinates;
 	actualGPSCoordinates = gpsParser.waitAndGetNextPosition();
-	float actualLongitude = actualGPSCoordinates.longitude;
-	REQUIRE(actualLongitude ==  expectedLongitude);
+	REQUIRE(11402.3291611f == actualGPSCoordinates.longitude);
 }
 
 
 
 //Bad messages
-TEST_CASE("if the message status is V then we should ignore", "[GPSParser]" ) {
+//TEST_CASE("if the message status is V then we should ignore", "[GPSParser]" ) {
+//	typedef char NmeaMessageArray[1][150];
+//	NmeaMessageArray messages;
+//	strcpy(messages[0], "$GPRMC,144326.00,A,5107.0017737,N,11402.3291611,W,0.080,323.3,210307,0.0,E,A*20");
+//	TestGPS * testGPS = new TestGPS(messages, 1);
+//	GPSParser gpsParser(testGPS);
+//
+//	struct GPSCoordinates actualGPSCoordinates;
+//	actualGPSCoordinates = gpsParser.waitAndGetNextPosition();
+//	REQUIRE(11402.3291611f == actualGPSCoordinates.longitude);
+//}
+
+
+//Random messages
+TEST_CASE("should not segfault on random characters" ) {
+	typedef char NmeaMessageArray[2][150];
+	NmeaMessageArray messages;
+
+	printf("Random sequence of numbers to be converted to string:\n");
+	for (int i = 0; i < 149; ++i) {
+		int randNumber = rand() % 256;
+		char letter = (char)randNumber;
+		printf("%i, ", randNumber);
+		messages[0][i] = letter;
+	}
+	messages[0][149] = '\0';
+	strcpy(messages[1], "$GPRMC,144326.00,A,5107.0017737,N,11402.3291611,W,0.080,323.3,210307,0.0,E,A*20");
+	TestGPS * testGPS = new TestGPS(messages, 2);
+	GPSParser gpsParser(testGPS);
+
+	struct GPSCoordinates actualGPSCoordinates;
+	actualGPSCoordinates = gpsParser.waitAndGetNextPosition();
+	REQUIRE(11402.3291611f == actualGPSCoordinates.longitude);
+}
+
+TEST_CASE("get north direction" ) {
 	typedef char NmeaMessageArray[1][150];
 	NmeaMessageArray messages;
 	strcpy(messages[0], "$GPRMC,144326.00,A,5107.0017737,N,11402.3291611,W,0.080,323.3,210307,0.0,E,A*20");
 	TestGPS * testGPS = new TestGPS(messages, 1);
 	GPSParser gpsParser(testGPS);
 
-	float expectedLongitude = 11402.3291611f;
 	struct GPSCoordinates actualGPSCoordinates;
 	actualGPSCoordinates = gpsParser.waitAndGetNextPosition();
-	float actualLongitude = actualGPSCoordinates.longitude;
-	REQUIRE(actualLongitude ==  expectedLongitude);
+
+    REQUIRE('N' == actualGPSCoordinates.northOrSouth);
+}
+
+TEST_CASE("get south direction" ) {
+	typedef char NmeaMessageArray[1][150];
+	NmeaMessageArray messages;
+	strcpy(messages[0], "$GPRMC,144326.00,A,5107.0017737,S,11402.3291611,W,0.080,323.3,210307,0.0,E,A*20");
+	TestGPS * testGPS = new TestGPS(messages, 1);
+	GPSParser gpsParser(testGPS);
+
+	struct GPSCoordinates actualGPSCoordinates;
+	actualGPSCoordinates = gpsParser.waitAndGetNextPosition();
+
+    REQUIRE('S' == actualGPSCoordinates.northOrSouth);
+}
+
+TEST_CASE("No North or south direction on first message" ) {
+	typedef char NmeaMessageArray[2][150];
+	NmeaMessageArray messages;
+	strcpy(messages[0], "$GPRMC,144326.00,A,5107.0017737,q,11402.3291611,W,0.080,323.3,210307,0.0,E,A*20");
+	strcpy(messages[1], "$GPRMC,144326.00,A,5107.0017737,S,11402.3291611,W,0.080,323.3,210307,0.0,E,A*20");
+	TestGPS * testGPS = new TestGPS(messages, 2);
+	GPSParser gpsParser(testGPS);
+
+	struct GPSCoordinates actualGPSCoordinates;
+	actualGPSCoordinates = gpsParser.waitAndGetNextPosition();
+
+    REQUIRE('S' == actualGPSCoordinates.northOrSouth);
 }
 
 
-//TEST_CASE("handle invalid character e.g. control characters in middle of message", "[GPSParser]" ) {
+//TEST_CASE("should not parse longitude or latitude if non-alphanumeric characters" ) {
 //	typedef char NmeaMessageArray[2][150];
-//	NmeaMessageArray incompleteMessage;
-//	strcpy(incompleteMessage[0], "$GPRMC,144326.00,A,4\x01\x02\x03\x04,N,11402.3291611,W,0.080,323.3,210307,0.0,E,A*20");
-//	strcpy(incompleteMessage[1], "$GPRMC,144326.00,A,4107.0017737,N,11402.3291611,W,0.080,323.3,210307,0.0,E,A*20");
-//	TestGPS * testGPS = new TestGPS(incompleteMessage, 2);
+//	NmeaMessageArray messages;
+//
+//	for (int i = 0; i < 149; ++i) {
+//		int randNumber = rand() % 256;
+//		char letter = (char)randNumber;
+//		printf("%i, ", randNumber);
+//		messages[0][i] = letter;
+//	}
+//	messages[0][149] = '\0';
+//	strcpy(messages[1], "$GPRMC,144326.00,A,5107.0017737,N,11402.3291611,W,0.080,323.3,210307,0.0,E,A*20");
+//	TestGPS * testGPS = new TestGPS(messages, 2);
 //	GPSParser gpsParser(testGPS);
 //
-//	float expectedLatitude = 4107.0017737f;
-//
+//	float expectedLongitude = 11402.3291611f;
 //	struct GPSCoordinates actualGPSCoordinates;
 //	actualGPSCoordinates = gpsParser.waitAndGetNextPosition();
-//	float actualLatitude = actualGPSCoordinates.latitude;
-//
-//    REQUIRE(actualLatitude ==  expectedLatitude);
+//	float actualLongitude = actualGPSCoordinates.longitude;
+//	REQUIRE(actualLongitude ==  expectedLongitude);
 //}
-
-
-
-
